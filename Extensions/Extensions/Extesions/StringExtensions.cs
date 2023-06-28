5616b07cb0ca36608db5d87af2415ade489adfdb
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-27 22:07:23 -0300

    First commit

---
+++ Extensions/Extensions/Extesions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Extension.Extesions
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return $"{thisObj.Substring(0, count)}...";
+            }
+        }
+    }
+}
---
+++ Extensions/Extensions/Program.cs
@@ -0,0 +1,20 @@
+﻿using Extension.Extesions;
+using System;
+
+namespace Extension;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2022, 11, 16, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+
+        Console.WriteLine();
+        string str = "Good morning dear students!";
+        Console.WriteLine(str.Cut(10));
+
+        Console.WriteLine();
+        string str1 = "Hello world!";
+        Console.WriteLine(str1.Cut(11));
+    }
+}
